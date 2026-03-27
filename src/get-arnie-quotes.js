d0335b021b3c55c5431494fb4dc6d7d88eb1b550
techgirl11 <shweta.mca1101@gmail.com>
2026-03-27 11:11:36 +1100

    getArnieQuotes implementation added

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,15 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const quoteResponse = await Promise.all(urls.map((url) => httpGet(url)));
+
+  return quoteResponse.map((result) => {
+    if (result.status === 200) {
+      return { "Arnie Quote": JSON.parse(result.body).message };
+    } else {
+      return { FAILURE: JSON.parse(result.body).message };
+    }
+  });
 };
 
 module.exports = {
